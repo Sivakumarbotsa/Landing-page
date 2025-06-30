b25de2d747c13913ba81e646f8a5bb831acc3256
Sivakumarbotsa <113580915+Sivakumarbotsa@users.noreply.github.com>
2025-06-30 13:53:34 +0530

    Initial commit for responsive landing page

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  const nav = document.getElementById("nav-links");
+  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+});
